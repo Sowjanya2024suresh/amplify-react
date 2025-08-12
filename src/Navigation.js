116c08124ea1ba5047276d1ad9bd45e15813498e
Sowjanya Ramanathan <sowjanya@lokas.info>
2025-08-12 22:23:21 +0530

    Update Navigation.js

--- src/Navigation.js
+++ src/Navigation.js
@@ -8,7 +8,7 @@ function Navigation() {
     <nav className={styles.navigation}>
       <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/6666a400ed624500c8676c81f096e0606829728a856b4cbc48be86b9beea6f1e?placeholderIfAbsent=true&apiKey=b009527b14c646d8a5f31ccb61a60ff1" alt="iGiver logo" className={styles.navLogo} />
       <ul className={styles.navList}>
-        <li className={styles.navItem}><a href="#home" className={styles.navLink}>Home</a></li>
+        // <li className={styles.navItem}><a href="#home" className={styles.navLink}>Home</a></li>
         <li className={styles.navItem}><a href="#methodology" className={styles.navLink}>Methodology</a></li>
         <li className={styles.navItem}><a href="#team" className={styles.navLink}>Team</a></li>
         <li className={styles.navItem}><a href="#impact" className={styles.navLink}>Impact</a></li>
